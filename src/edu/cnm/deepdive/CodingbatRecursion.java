e73f112c0cfed7971123d09a9b04b7d10b64c3fd
Jose Ortiz <jose.ortiz@live.com>
2020-10-13 19:06:13 -0600

    Added second recursion problem. Needs to add test.

--- src/edu/cnm/deepdive/CodingbatRecursion.java
+++ src/edu/cnm/deepdive/CodingbatRecursion.java
@@ -6,5 +6,12 @@ public class CodingbatRecursion {
     return n * factorial(n-1);
   }
 
+  public int bunnyEars(int bunnies) {
+    if (bunnies == 0) {
+     return bunnies;
+    }
+    return 2 + bunnyEars(bunnies - 1);
+  }
+
 
 }
